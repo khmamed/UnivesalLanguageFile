d222aacccac69cbb4b07f85853af8595af18c49e
khmamed <khmmamed@gmail.com>
2017-01-20 00:11:46 +0000

    Update UFL.js

--- UFL.js
+++ UFL.js
@@ -1,36 +1,35 @@
-/****
- * created by khm@med 
- * 19/01/2017
- ***/
+/**** created by khm@med.
+    * 19/01/2017
+    */
 
 
 
 exports.Lang = {
 
-    home : { Ar : "الرئيسية", Fr : "", En : ""},
-    about : { Ar : "", Fr : "", En : ""},
-    contact : { Ar : "اتصل بنا", Fr : "", En : ""},
-    signUp : { Ar : "تسجيل", Fr : "", En : ""},
+    home : { Ar : "الرئيسية", Fr : "Acceuil", En : "Home"},
+    about : { Ar : "", Fr : "", En : "About"},
+    contact : { Ar : "اتصل بنا", Fr : "", En : "Contact us"},
+    signUp : { Ar : "تسجيل", Fr : "Enregistrez-Vous", En : "S"},
     signIn : { Ar : "تسجيل الدخول", Fr : "", En : ""},
     register : { Ar : "افتح حساب", Fr : "", En : ""},
-    registred : { Ar : "مسحل", Fr : "", En : ""},
+    registered : { Ar : "مسحل", Fr : "", En : ""},
     userName : { Ar : "اسم المستخدم", Fr : "", En : ""},
     password : { Ar : "كلمة المرور", Fr : "", En : ""},
     email : { Ar : "اﻹسم الكامل", Fr : "", En : ""},
     confirmPassword : { Ar : "تأكيد كلمة المرور", Fr : "", En : ""},
-    cancel : { Ar : "", Fr : "", En : ""},
+    cancel : { Ar : "الغاء", Fr : "", En : ""},
     guide : { Ar : "", Fr : "", En : ""},
     documentation : { Ar : "", Fr : "", En : ""},
-    language : { Ar : "", Fr : "", En : ""},
-    welcome : { Ar : "", Fr : "", En : ""},
-    rememberMe : { Ar : "", Fr : "", En : ""},
-    account : { Ar : "", Fr : "", En : ""},
-    createAccount : { Ar : "", Fr : "", En : ""},
-    forgotPassword : { Ar : "", Fr : "", En : ""},
-    firstName : { Ar : "", Fr : "", En : ""},
-    lastName : { Ar : "", Fr : "", En : ""},
-    name : { Ar : "", Fr : "", En : ""},
-    fullName : { Ar : "", Fr : "", En : ""}
+    language : { Ar : "اللغة", Fr : "", En : ""},
+    welcome : { Ar : "مرحبا", Fr : "", En : ""},
+    rememberMe : { Ar : "تذكرني", Fr : "", En : ""},
+    account : { Ar : "الحساب", Fr : "Compte", En : "Account"},
+    createAccount : { Ar : "انشاء حساب", Fr : "Creer un compte", En : "Create an account"},
+    forgotPassword : { Ar : "هل نسيت كلمة السر", Fr : "", En : ""},
+    firstName : { Ar : "الاسم الشخصي", Fr : "Prenom", En : ""},
+    lastName : { Ar : "الاسم العائلي", Fr : "Nom", En : ""},
+    name : { Ar : "الاسم", Fr : "Nom", En : "Name"},
+    fullName : { Ar : "الاسم الكامل", Fr : "Nom complet", En : "Full name"}
 
     
 };
